9ba60dc6861c93ec7bdb5a1eed63d21eac048dbb
Anders Gustafsson <andersgee@gmail.com>
2022-08-18 17:32:52 +0200

    sharing Vec<f32> with js as Float32Array

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
-import init, { add } from "./pkg/hello_wasm.js";
+import init, { add, Example } from "./pkg/rust_wasm_hello_world.js";
+//import { memory } from "./pkg/rust_wasm_hello_world_bg.wasm.d.ts";
+
+/**
+ * view into the linear webassembly memory.
+ *
+ * note: be reasonable with this.
+ * for example: calling .free() on a struct that gave the ptr will make this array reference garbage.
+ * todo: find out under which conditions this becomes garbage. probably any and all allocation in the specific struct?
+ * also: there nothing preventing us from writing outside the array bounds... corrupting whatever comes after.
+ */
+function f32arrayFromMemory(buffer, ptr, len) {
+  return new Float32Array(buffer, ptr, len);
+}
 
 async function main() {
-  await init();
+  const wasm = await init();
 
   console.log(`The answer is ${add(40, 2)}`);
+  const buf = wasm.memory.buffer;
+  const example = new Example();
+  console.log(example);
+
+  console.log(example.my_number);
+  example.my_number = 99;
+
+  console.log(example.my_number);
+  console.log(example.my_vec);
+
+  const kek = example.my_vec;
+  console.log(example.my_vec);
+
+  kek[1] = 919221;
+  console.log(example.my_vec);
+
+  console.log(kek.length);
+
+  example.my_vec[2] = 11111;
+  console.log(example.my_vec);
+
+  example.my_vec = new Float32Array([91, 83, 2]);
+  console.log(example.my_vec);
+
+  console.log(example.sum_my_vec());
+
+  example.my_vec = [1, 3, 5, 99];
+  console.log(example.sum_my_vec());
 }
 
 main();
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,72 @@
+use js_sys::Float32Array;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(a:i32, b:i32)->i32 {
-    a+b
+extern "C" {
+  #[wasm_bindgen(js_namespace = console)]
+  fn log(s: &str);
+
+  #[wasm_bindgen(js_namespace = Math)]
+  fn random() -> f64;
 }
 
+#[wasm_bindgen]
+pub struct Example {
+  my_number: f64,
+  my_vec: Vec<f32>,
+}
 
-/* 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[wasm_bindgen]
+impl Example {
+  #[wasm_bindgen(constructor)]
+  pub fn new() -> Example {
+    log("instantiating new Example");
+
+    Example {
+      my_number: random(),
+      my_vec: vec![0.1, 0.2, 9.56],
+    }
+  }
+
+  #[wasm_bindgen(getter)]
+  pub fn my_number(&self) -> f64 {
+    self.my_number
+  }
+
+  #[wasm_bindgen(setter)]
+  pub fn set_my_number(&mut self, my_number: f64) {
+    self.my_number = my_number;
+  }
+
+  #[wasm_bindgen(getter)]
+  pub fn my_vec(&self) -> Float32Array {
+    unsafe { Float32Array::view(&self.my_vec) }
+  }
+
+  #[wasm_bindgen(setter)]
+  pub fn set_my_vec(&mut self, my_vec: Float32Array) {
+    for i in 0..self.my_vec.len() {
+      self.my_vec[i] = my_vec.at(i.try_into().unwrap()).unwrap_or(0.);
+    }
+  }
+
+  pub fn sum_my_vec(&self) -> f32 {
+    self.my_vec.iter().sum()
+  }
+}
+
+#[wasm_bindgen]
+pub fn add(a: i32, b: i32) -> i32 {
+  a + b
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+  #[test]
+  fn it_works() {
+    let result = add(2, 2);
+    assert_eq!(result, 4);
+  }
 }
-*/
